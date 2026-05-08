9b1bc6d6273e1813befae12c7ef4ac0fc0391980
Elcris1 <eloicp10@gmail.com>
2026-05-08 20:15:43 +0200

    feat(functions/service): call tokens functions and send notifications

--- app/src/main/java/com/example/ecofruit/ui/services/MessagingService.kt
+++ app/src/main/java/com/example/ecofruit/ui/services/MessagingService.kt
@@ -11,11 +11,24 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.example.ecofruit.R
 import com.example.ecofruit.ui.activities.LauncherActivity
+import com.example.ecofruit.ui.data.repository.SettingsRepository
+import com.example.ecofruit.ui.data.repository.UserRepository
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class MessagingService: FirebaseMessagingService() {
+    private val userRepo = UserRepository.getInstance()
+    private lateinit var settingsRepo: SettingsRepository
+
+    override fun onCreate() {
+        super.onCreate()
+        settingsRepo = SettingsRepository(applicationContext)
+    }
+
     // [START receive_message]
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         // TODO(developer): Handle FCM messages here.
@@ -25,13 +38,12 @@ class MessagingService: FirebaseMessagingService() {
         // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
             Log.d(TAG, "Message data payload: ${remoteMessage.data}")
-
-
         }
 
         // Check if message contains a notification payload.
         remoteMessage.notification?.let {
             Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.body ?: "")
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
@@ -53,15 +65,19 @@ class MessagingService: FirebaseMessagingService() {
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // FCM registration token to your app server.
-        sendRegistrationToServer(token)
+        CoroutineScope(Dispatchers.IO).launch {
+            settingsRepo.settingsFlow.collect { settings ->
+                settings.fcmToken?.let {
+                    userRepo.updateFcmTokenStatus(settings.fcmToken, false)
+                }
+            }
+            // Persist the token locally
+            settingsRepo.setFcmToken(token)
+            
+            userRepo.saveFcmToken(token)
+        }
     }
     // [END on_new_token]
-
-
-    private fun sendRegistrationToServer(token: String?) {
-        // TODO: Implement this method to send token to your app server.
-        Log.d(TAG, "sendRegistrationTokenToServer($token)")
-    }
 
     private fun sendNotification(messageBody: String) {
         val intent = Intent(this, LauncherActivity::class.java)
@@ -78,7 +94,7 @@ class MessagingService: FirebaseMessagingService() {
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle("FCM Message")
+            .setContentTitle(getString(R.string.app_name))
             .setContentText(messageBody)
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
@@ -90,13 +106,13 @@ class MessagingService: FirebaseMessagingService() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 channelId,
-                "Channel human readable title",
+                "EcoFruit Notifications",
                 NotificationManager.IMPORTANCE_DEFAULT,
             )
             notificationManager.createNotificationChannel(channel)
         }
 
-        val notificationId = 0
+        val notificationId = System.currentTimeMillis().toInt()
         notificationManager.notify(notificationId, notificationBuilder.build())
     }
 
